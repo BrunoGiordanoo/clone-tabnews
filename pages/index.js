5e435bcd4c357dced542f8ea35a65e5b8948428d
Bruno Giordano <brunogiordanoopo@gmail.com>
2026-02-21 20:43:07 +0000

    adiciona `scripts de migracao`

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/construction.module.css";
 export default function Home() {
   return (
     <main className={styles.container}>
-      <h1 className={styles.title}>Em construção</h1>
+      <h1 className={styles.title}>Em construção!</h1>
       <p className={styles.subtitle}>Voltamos em breve.</p>
     </main>
   );
